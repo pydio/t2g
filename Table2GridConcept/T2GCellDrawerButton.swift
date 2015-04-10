beeb390980e0165a53f450bdb7106042c15c8785
Michal Svacha <michal.svacha@gmail.com>
2015-04-10 18:41:45 +0200

    Extraction of coloring background of button in new class

--- Table2GridConcept/T2GCellDrawerButton.swift
+++ Table2GridConcept/T2GCellDrawerButton.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class T2GCellButton: UIButton {
+class T2GCellDrawerButton: T2GColoredButton {
     var minOriginCoord: CGPoint?
     var maxOriginCoord: CGPoint? {
         get {
@@ -23,44 +23,15 @@ class T2GCellButton: UIButton {
         }
     }
     
-    var normalBackgroundColor: UIColor? {
-        didSet {
-            self.backgroundColor = normalBackgroundColor!
-        }
-    }
-    var highlightedBackgroundColor: UIColor?
-    
-    /*
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func drawRect(rect: CGRect) {
-        // Drawing code
-    }
-    */
-    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         self.minSize = frame.size.width
         self.minOriginCoord = frame.origin
     }
-
-    required init(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
     
-    func didTap() {
-        self.backgroundColor = self.highlightedBackgroundColor!
-    }
-    
-    func didUntap() {
-        self.backgroundColor = self.normalBackgroundColor!
-    }
-    
-    func setup() {
-        self.addTarget(self, action: "didTap", forControlEvents: UIControlEvents.TouchDown)
-        self.addTarget(self, action: "didUntap", forControlEvents: UIControlEvents.TouchUpInside)
-        self.addTarget(self, action: "didUntap", forControlEvents: UIControlEvents.TouchUpOutside)
+    override func setup() {
+        super.setup()
         self.setTitle("\(self.tag - T2GViewTags.cellDrawerButtonConstant.rawValue + 1)", forState: UIControlState.Normal)
     }
     
---
+++ Table2GridConcept/T2GColoredButton.swift
@@ -0,0 +1,33 @@
+//
+//  T2GColoredButton.swift
+//  Table2GridConcept
+//
+//  Created by Michal Švácha on 10/04/15.
+//  Copyright (c) 2015 Michal Švácha. All rights reserved.
+//
+
+import UIKit
+
+class T2GColoredButton: UIButton {
+    var normalBackgroundColor: UIColor? {
+        didSet {
+            self.backgroundColor = normalBackgroundColor!
+        }
+    }
+    var highlightedBackgroundColor: UIColor?
+    
+    func didTap() {
+        self.backgroundColor = self.highlightedBackgroundColor!
+    }
+    
+    func didUntap() {
+        self.backgroundColor = self.normalBackgroundColor!
+    }
+    
+    func setup() {
+        self.addTarget(self, action: "didTap", forControlEvents: UIControlEvents.TouchDown)
+        self.addTarget(self, action: "didUntap", forControlEvents: UIControlEvents.TouchUpInside)
+        self.addTarget(self, action: "didUntap", forControlEvents: UIControlEvents.TouchUpOutside)
+        //self.setTitle("\(self.tag - T2GViewTags.cellDrawerButtonConstant.rawValue + 1)", forState: UIControlState.Normal)
+    }
+}
