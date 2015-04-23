48148ea17eb4210a4b34a9241a50648c5d63b983
Michal Svacha <michal.svacha@gmail.com>
2015-04-23 11:54:22 +0200

    Implementation of the new scheme (layoutMode in T2GScrollView)

--- Examples/SplitView/SplitView/ExampleViewController.swift
+++ Examples/SplitView/SplitView/ExampleViewController.swift
@@ -97,26 +97,26 @@ class ExampleViewController: T2GViewController, T2GViewControllerDelegate, T2GDr
         var view: T2GCell?
         switch(indexPath.section) {
         case 0:
-            view = T2GCell(header: "R: \(self.modelArray[indexPath.row]) | S: \(indexPath.section) | T: \(self.scrollView.indexForIndexPath(indexPath) + T2GViewTags.cellConstant.rawValue)", detail: "\(indexPath)", frame: frame, mode: self.layoutMode)
-            view!.setupButtons(indexPath.row%5, mode: self.layoutMode)
+            view = T2GCell(header: "R: \(self.modelArray[indexPath.row]) | S: \(indexPath.section) | T: \(self.scrollView.indexForIndexPath(indexPath) + T2GViewTags.cellConstant.rawValue)", detail: "\(indexPath)", frame: frame, mode: self.scrollView.layoutMode)
+            view!.setupButtons(indexPath.row%5, mode: self.scrollView.layoutMode)
             view!.draggable = true
             view!.draggableDelegate = self
             break
         case 1:
-            view = T2GCell(header: "R: \(self.modelArray2[indexPath.row]) | S: \(indexPath.section) | T: \(self.scrollView.indexForIndexPath(indexPath) + T2GViewTags.cellConstant.rawValue)", detail: "\(indexPath)", frame: frame, mode: self.layoutMode)
-            view!.setupButtons(indexPath.row%5, mode: self.layoutMode)
+            view = T2GCell(header: "R: \(self.modelArray2[indexPath.row]) | S: \(indexPath.section) | T: \(self.scrollView.indexForIndexPath(indexPath) + T2GViewTags.cellConstant.rawValue)", detail: "\(indexPath)", frame: frame, mode: self.scrollView.layoutMode)
+            view!.setupButtons(indexPath.row%5, mode: self.scrollView.layoutMode)
             view!.draggable = true
             view!.draggableDelegate = self
             break
         case 2:
-            view = T2GCell(header: "R: \(self.modelArray3[indexPath.row]) | S: \(indexPath.section) | T: \(self.scrollView.indexForIndexPath(indexPath) + T2GViewTags.cellConstant.rawValue)", detail: "\(indexPath)", frame: frame, mode: self.layoutMode)
-            view!.setupButtons(indexPath.row%5, mode: self.layoutMode)
+            view = T2GCell(header: "R: \(self.modelArray3[indexPath.row]) | S: \(indexPath.section) | T: \(self.scrollView.indexForIndexPath(indexPath) + T2GViewTags.cellConstant.rawValue)", detail: "\(indexPath)", frame: frame, mode: self.scrollView.layoutMode)
+            view!.setupButtons(indexPath.row%5, mode: self.scrollView.layoutMode)
             view!.draggable = true
             view!.draggableDelegate = self
             break
         default:
-            view = T2GCell(header: "", detail: "\(indexPath)", frame: frame, mode: self.layoutMode)
-            view!.setupButtons(indexPath.row%5, mode: self.layoutMode)
+            view = T2GCell(header: "", detail: "\(indexPath)", frame: frame, mode: self.scrollView.layoutMode)
+            view!.setupButtons(indexPath.row%5, mode: self.scrollView.layoutMode)
             view!.draggable = true
             view!.draggableDelegate = self
             break
