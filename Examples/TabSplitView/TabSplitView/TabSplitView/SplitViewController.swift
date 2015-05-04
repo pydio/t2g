0a73566d0e69a2476b65edd8a852a22930767a97
Michal Svacha <michal.svacha@gmail.com>
2015-05-04 14:10:42 +0200

    Added custom splitVC class

---
+++ Examples/TabSplitView/TabSplitView/TabSplitView/SplitViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SplitViewController.swift
+//  SplitView
+//
+//  Created by Michal Švácha on 16/04/15.
+//  Copyright (c) 2015 Michal Švácha. All rights reserved.
+//
+
+import UIKit
+
+class SplitViewController: UISplitViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
