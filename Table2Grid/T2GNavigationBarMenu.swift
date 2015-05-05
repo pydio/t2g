57dc0da69e55696068fb4d7d93aeaa5db682480d
Michal Svacha <michal.svacha@gmail.com>
2015-05-05 15:46:39 +0200

    Comments

--- Table2Grid/T2GNavigationBarMenu.swift
+++ Table2Grid/T2GNavigationBarMenu.swift
@@ -9,28 +9,28 @@
 import UIKit
 
 /**
-...
+Protocol for T2GNavigationBarMenu items' appearance and for handling clicking event.
 */
 protocol T2GNavigationBarMenuDelegate {
     /**
-    ...
+    Gets called when the menu is being created.
     
-    :param: index
-    :param: size
-    :returns:
+    :param: index Index of the cell in the menu. Indexed from 0 from the top.
+    :param: size Size of the whole cell.
+    :returns: UIView object that will be put as a subview of the button.
     */
     func viewForCell(index: Int, size: CGSize) -> UIView
     
     /**
-    ...
+    Gets called when a button has been pressed.
     
-    :param: index
+    :param: index Index of the selected button.
     */
     func didSelectButton(index: Int)
 }
 
 /**
-...
+Class for menu view sliding from below the navigation bar.
 */
 class T2GNavigationBarMenu: UIView {
     var delegate: T2GNavigationBarMenuDelegate?
@@ -38,7 +38,11 @@ class T2GNavigationBarMenu: UIView {
     var setCount: Int = 0
     
     /**
-    ...
+    Custom initializer - initializes the menu and creates all its subviews. If delegate isn't set the cells will be empty.
+    
+    :param: frame Default Cocoa API - The frame rectangle for the view, measured in points.
+    :param: itemCount Expected number of items. Their size is calculated proportionally
+    :param: delegate Delegate object that will determine appearance of the items in the menu and handle their events.
     */
     convenience init(frame: CGRect, itemCount: Int, delegate: T2GNavigationBarMenuDelegate?) {
         self.init(frame: frame)
@@ -61,6 +65,7 @@ class T2GNavigationBarMenu: UIView {
             }
             self.addSubview(button)
             
+            /// separators
             if index + 1 < itemCount {
                 let offset: CGFloat = 30.0
                 let line = UIView(frame: CGRectMake(offset, button.frame.origin.y + button.frame.size.height, button.frame.size.width - offset, 1))
@@ -71,11 +76,11 @@ class T2GNavigationBarMenu: UIView {
     }
     
     /**
-    ...
+    Redirects button action to its delegate.
     
-    :param: sender
+    :param: sender The button on which the action has been called.
     */
-    func buttonClicked(sender: T2GColoredButton) {
+    internal func buttonClicked(sender: T2GColoredButton) {
         self.delegate?.didSelectButton(sender.tag)
     }
 }
