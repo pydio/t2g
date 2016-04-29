e54cf147ccce1d1b4dd1fc5c1cf5dd59ded701cb
leobouilloux <leo,bouilloux@gmail.com>
2016-04-29 11:58:27 +0200

    Added CopyMoveView style attributes

--- Table2Grid/T2GStyle.swift
+++ Table2Grid/T2GStyle.swift
@@ -12,7 +12,7 @@ import UIKit
 
 
 struct T2GStyle {
-    // Nodes
+
     struct Node {
         static var nodeTitleColor = UIColor.blackColor()
         static var nodeTitleFont = UIFont(name: "SFUIDisplay-Regular", size: 16)
@@ -23,5 +23,13 @@ struct T2GStyle {
         
         static var nodeIconViewBackgroundColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.02)
         static var nodeImageViewTintColor = UIColor.darkGrayColor()
+    }
+    
+    struct CopyMoveView {
+        static var backgroundColor = UIColor(named: .PYDMarine)
+        static var titleFont = UIFont(name: "SFUIDisplay-Light", size: 26)
+        static var titleColor = UIColor.whiteColor()
+        static var itemFont = UIFont(name: "SFUIDisplay-Regular", size: 15)
+        static var itemColor = UIColor.whiteColor()
     }
 }
