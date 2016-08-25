5d527dcdf39ba4d4c128c6d686754b8e73a1a006
leobouilloux <leo,bouilloux@gmail.com>
2016-08-25 16:09:02 +0200

    Each element in T2GStyle is now public.

--- Table2Grid/Classes/T2GStyle.swift
+++ Table2Grid/Classes/T2GStyle.swift
@@ -11,29 +11,29 @@ import UIKit
 
 public struct T2GStyle {
 
-    struct Node {
-        static var nodeTitleColor = UIColor.blackColor()
-        static var nodeTitleFont = UIFont(name: "SFUIDisplay-Regular", size: 16)
+    public struct Node {
+        public static var nodeTitleColor = UIColor.blackColor()
+        public static var nodeTitleFont = UIFont(name: "SFUIDisplay-Regular", size: 16)
         
-        static var nodeDescriptionColor = UIColor.grayColor()
-        static var nodeDescriptionFont = UIFont(name: "SFUIDisplay-Light", size: 13)
+        public static var nodeDescriptionColor = UIColor.grayColor()
+        public static var nodeDescriptionFont = UIFont(name: "SFUIDisplay-Light", size: 13)
         
-        static var nodeIconViewBackgroundColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.02)
-        static var nodeImageViewTintColor = UIColor(red: 119/255, green: 119/255, blue: 119/255, alpha: 1.0)
-        static var nodeBackgroundViewBackgroundColor = UIColor.whiteColor()
-        static var nodeScrollViewBackgroundColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.02)
+        public static var nodeIconViewBackgroundColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.02)
+        public static var nodeImageViewTintColor = UIColor(red: 119/255, green: 119/255, blue: 119/255, alpha: 1.0)
+        public static var nodeBackgroundViewBackgroundColor = UIColor.whiteColor()
+        public static var nodeScrollViewBackgroundColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.02)
         
 
         
         
         
-        struct Collection {
-            static var backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.04)
-            static var whiteFooterBackgroundColor = UIColor.whiteColor()
+        public struct Collection {
+            public static var backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.04)
+            public static var whiteFooterBackgroundColor = UIColor.whiteColor()
         }
         
-        struct Table {
-            static var backgroundColor = UIColor(red: 0.995, green: 0.995, blue: 0.995, alpha: 1)
+        public struct Table {
+            public static var backgroundColor = UIColor(red: 0.995, green: 0.995, blue: 0.995, alpha: 1)
         }
         
     }
