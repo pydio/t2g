86d4c948f6d535713f754ec05c59595a4666ebc6
leobouilloux <leo,bouilloux@gmail.com>
2016-10-07 10:42:27 +0200

    Fixed padding problem for annotations

--- Table2Grid/Classes/T2GCell.swift
+++ Table2Grid/Classes/T2GCell.swift
@@ -392,7 +392,7 @@ open class T2GCell: T2GDragAndDropView, UIScrollViewDelegate {
         imageView.addSubview(infoView)
         imageView.addConstraints([ // INFOVIEW
             NSLayoutConstraint(item: infoView, attribute: .width, relatedBy: .equal, toItem: imageView, attribute: .width, multiplier: 1, constant: 0),
-            NSLayoutConstraint(item: infoView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 20),
+            NSLayoutConstraint(item: infoView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 22),
             NSLayoutConstraint(item: infoView, attribute: .bottom, relatedBy: .equal, toItem: imageView, attribute: .bottom, multiplier: 1, constant: 0),
             NSLayoutConstraint(item: infoView, attribute: .centerX, relatedBy: .equal, toItem: imageView, attribute: .centerX, multiplier: 1, constant: 0),
             ])
